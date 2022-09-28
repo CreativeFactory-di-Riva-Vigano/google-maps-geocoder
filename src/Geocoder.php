5c804138c388e77412bb77674881b99182485566
daniele-creativefactory <86785881+daniele-creativefactory@users.noreply.github.com>
2022-09-28 16:50:54 +0200

    Added cache support.

--- src/Geocoder.php
+++ src/Geocoder.php
@@ -2,6 +2,8 @@
 
 namespace CreativeFactoryRV\GoogleMapsGeocoder;
 
+use Psr\Cache\CacheItemPoolInterface;
+
 class Geocoder {
     /** @var string */
     private $language;
@@ -9,21 +11,42 @@ class Geocoder {
     /** @var string */
     private $apiKey;
 
+    private $cacheAdapter;
+
     /**
-     * @param $apiKey Google Maps Platform API key obtained from Google.
-     * @param $language Results langage.
+     * @param string $apiKey Google Maps Platform API key obtained from Google.
+     * @param string $language Results langage.
+     * @param CacheItemPoolInterface|null $cacheAdapter PSR-6 cache adapter implementing CacheItemPoolInterface.
      */
-    public function __construct($apiKey, $language = 'en') {
+    public function __construct(string $apiKey, string $language = 'en', CacheItemPoolInterface $cacheAdapter = null) {
         $this->apiKey = $apiKey;
         $this->language = $language;
+        $this->cacheAdapter = $cacheAdapter;
     }
 
     /**
-     * @param $address The address we want to be geocoded.
+     * @param string $address The address we want to be geocoded.
      * @return Location Location object istantiated with the information retrieved from Google Maps Platform.
      * @throws \Exception If something goes wrong, it throws an exception with information about the problem.
      */
-    public function query($address): Location {
+    public function query(string $address): Location {
+        if ( $this->cacheAdapter != null ) {
+            $cacheIndex = md5(__CLASS__ . __FUNCTION__ . '|' . serialize(func_get_args()));
+            try {
+                $item = $this->cacheAdapter->getItem($cacheIndex);
+                $isHit = $item->isHit();
+                if ( $isHit ) {
+                    return $item->get();
+                }
+                else {
+                    $doCaching = true;
+                }
+            }
+            catch (\Exception $e) {
+                $doCaching = false;
+            }
+        }
+
         $url = 'https://maps.google.com/maps/api/geocode/json?address=' . urlencode($address) . '&key=' . $this->apiKey . '&language=' . $this->language;
 
         $curl = curl_init();
@@ -33,6 +56,13 @@ class Geocoder {
         $data = curl_exec($curl);
         curl_close($curl);
 
-        return new Location($data);
+        $location = new Location($data);
+
+        if ( $doCaching && ($this->cacheAdapter != null) ) {
+            $item->set($location);
+            $this->cacheAdapter->save($item);
+        }
+
+        return $location;
     }
 }
